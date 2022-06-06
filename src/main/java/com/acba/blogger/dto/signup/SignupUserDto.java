b2656c267d1b4b655b62be1adbd2c632c4916eb2
Sargis Sargsyan <sargiss987@gmail.com>
2022-06-06 10:39:48 +0400

    issue#22:have been added validation rules

--- src/main/java/com/acba/blogger/dto/signup/SignupUserDto.java
+++ src/main/java/com/acba/blogger/dto/signup/SignupUserDto.java
@@ -1,10 +1,23 @@
 package com.acba.blogger.dto.signup;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class SignupUserDto {
 
+  private static final String PASSWORD_PATTERN =
+      "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#$%^&*()_{}]).{8,20})";
+
+  @NotBlank(message = "{validation.firstName.error.message}")
   private final String firstName;
+
+  @NotBlank(message = "{validation.lastName.error.message}")
   private final String lastName;
+
+  @NotBlank(message = "{validation.email.error.message}")
   private final String email;
+
+  @Pattern(regexp = PASSWORD_PATTERN, message = "{validation.password.error.message}")
   private final String password;
 
   public SignupUserDto(String firstName, String lastName, String email, String password) {
--- src/main/java/com/acba/blogger/dto/user/CreateUserDto.java
+++ src/main/java/com/acba/blogger/dto/user/CreateUserDto.java
@@ -1,10 +1,26 @@
 package com.acba.blogger.dto.user;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class CreateUserDto {
+
+  private static final String PASSWORD_PATTERN =
+      "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#$%^&*()_{}]).{8,20})";
+
+  @NotBlank(message = "{validation.firstName.error.message}")
   private final String firstName;
+
+  @NotBlank(message = "{validation.lastName.error.message}")
   private final String lastName;
+
+  @NotBlank(message = "{validation.email.error.message}")
   private final String email;
+
+  @Pattern(regexp = PASSWORD_PATTERN, message = "{validation.password.error.message}")
   private final String password;
+
+  @NotBlank(message = "{validation.role.error.message}")
   private final String roleType;
 
   public CreateUserDto(
